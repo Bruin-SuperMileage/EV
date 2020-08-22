68d771412f4e11c917f29e3d47cb66eba561029c
ashvinnagarajan <50090261+ashvinnagarajan@users.noreply.github.com>
2020-08-21 17:49:52 -0700

    Address PR comments

--- dyno/panda/dyno_panel.py
+++ dyno/panda/dyno_panel.py
@@ -5,8 +5,10 @@ import serial
 import time
 
 #Define constants
-NUMSENSORS1 = 2 #RPM AND TEMPERATURE
-NUMSENSORS2 = 2 #JOULEMETER
+LEXI_NUMSENSORS = 2 #RPM AND TEMPERATURE
+OLIVER_NUMSENSORS = 2 #JOULEMETER
+READ_SERIAL_ON = False
+loop_speed = 100 #ms
 #---------------------
 
 #Images
@@ -21,49 +23,57 @@ color_label = "#B1D9DB"
 #---------------------
 
 #Placement
-LABEL_COL1_PLACE = {'relx':0.15, 'rely':0.6}
-LABEL_COL2_PLACE = {'relx':0.55, 'rely':0.6}
-LABEL_SPACE = 0.08
+LABEL_COL1_PLACE = {'relx':0.15, 'rely':0.45}
+LABEL_COL2_PLACE = {'relx':0.55, 'rely':0.4}
+LABEL_SPACE = 0.07
 #---------------------
 
 #Open the serial port that the MCU is connected to
-'''ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=0.5)'''
+if READ_SERIAL_ON:
+    t_out = 0.5
+    lexi = serial.Serial('/dev/ttyUSB0', 9600, timeout=t_out)
+    oliver = serial.Serial('/dev/ttyUSB1', 9600, timeout=t_out)
+    driver = serial.Serial('/dev/ttyUSB2', 9600, timeout=t_out)
 #---------------------
 
 #Update the variables for the labels
 def update_labels():
     #Read in input from MCU sensors
-    '''for x in range (0, NUMSENSORS1): #read sensor statements
-        val = ser1.readline().decode('ascii')
-        prefix = val[0:3]
-        my_dict[prefix] = float(val[5:-2])
-    for x in range (0, NUMSENSORS2): #read JOULEMETER statements
-        val = ser2.readline().decode('ascii')
-        prefix = val[0:3]
-        my_dict[prefix] = float(val[5:-2])'''
-    for key in my_dict.keys():
-        my_dict[key][1] = my_dict[key][1] + 1
+    if READ_SERIAL_ON:
+        for x in range (0, LEXI_NUMSENSORS): #read Lexi sensor data
+            val = lexi.readline().decode('ascii')
+            prefix = val[0:3]
+            my_dict[prefix] = float(val[5:-2])
+        for x in range (0, OLIVER_NUMSENSORS): #read Oliver sensor data
+            val = oliver.readline().decode('ascii')
+            prefix = val[0:3]
+            my_dict[prefix] = float(val[5:-2])
+    else:
+        for key in my_dict.keys():
+            my_dict[key][1] = my_dict[key][1] + 1
+    
     my_list.append(str(my_dict))
 
     temp = list(my_dict.values())
-    
     for i in range(num_vars):
         var_array[i].set(temp[i][0] + str(temp[i][1]) + temp[i][2])
 
-    window.after(100, update_labels)
+    window.after(loop_speed, update_labels)
 #---------------------
 
 #Define on_click function for button
 def handle_click(event):
     if button["text"] == "Pause":
         button["text"] = "Run"
-        #ser.write('P;')
+        if READ_SERIAL_ON:
+            driver.write('P;')
         print("Flag was set to 'P;'")
         writeFile()
         update_labels()
     elif button["text"] == "Run":
         button["text"] = "Pause"
-        #ser.write('R;')
+        if READ_SERIAL_ON:
+            driver.write('R;')
         print("Flag was set to 'R;'")
 #---------------------
 
@@ -78,9 +88,9 @@ def writeFile():
 #Create the viewable window
 window = tk.Tk()
 window.configure(bg= color_bg)
-window.geometry("800x500")
-font_title = font.Font(family="Helvetia", size=36, weight="bold")
-font_text = font.Font(family="Helvetia", size=24)
+window.geometry("800x600")
+font_title = font.Font(family="Helvetica", size=36, weight="bold")
+font_text = font.Font(family="Helvetica", size=24)
 #---------------------
 
 #Initialize variables
@@ -113,21 +123,17 @@ label.place(relx = 0.5, rely = 0.21, anchor='center')
 #---------------------
 
 #Create the start/stop button
-start_img = ImageTk.PhotoImage(Image.open(start_path).resize([100, 100]))
+start_img = ImageTk.PhotoImage(Image.open(start_path).resize([70, 70]))
 button = tk.Button(bg = color_bg, activebackground = color_bg, text = "Pause", image = start_img, borderwidth = 0)
-button.place(relx = 0.5, rely = 0.4, anchor='center')
+button.place(relx = 0.5, rely = 0.35, anchor='center')
 button.bind("<Button-1>", handle_click)
 #---------------------
 
 #Create labels to display sensor readings
-for i in range(int(num_vars/2)): #First Column
+for i in range(int(num_vars)): #First Column
     label1 = tk.Label(window, textvariable=var_array[i], bg=color_bg, fg=color_label, font=font_text)
     label1.place(relx = LABEL_COL1_PLACE['relx'], rely = LABEL_COL1_PLACE['rely']+i*LABEL_SPACE, anchor='w')
 
-for i in range(int(num_vars/2), num_vars): #Second Column
-    label2 = tk.Label(window, textvariable=var_array[i], bg=color_bg, fg=color_label, font=font_text)
-    label2.place(relx = LABEL_COL2_PLACE['relx'], rely = LABEL_COL2_PLACE['rely']+(i-4)*LABEL_SPACE, anchor='w')
-
-window.after(100, update_labels)
+window.after(loop_speed, update_labels)
 window.mainloop()
 #---------------------
