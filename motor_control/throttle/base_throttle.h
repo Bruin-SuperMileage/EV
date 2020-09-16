a7eba72e8711746431d1e552a98b2e335bff4d24
rgimel <56706235+rgimel@users.noreply.github.com>
2020-09-16 16:17:20 -0700

    throttle as a class

--- motor_control/throttle/base_throttle.h
+++ motor_control/throttle/base_throttle.h
@@ -1,13 +1,24 @@
 #ifndef BASE_THR_H
 #define BASE_THR_H
 
+//#include <Servo.h>
+#define throttleIn 5   // Placeholders
+#define ESCOutput 3
+#define button 7
+
 class Throttle
 {
 public:
-    Throttle(const int &pin);
+    Throttle();
+
+    void run();
+    void setup();
+    void loop();
 
     virtual void compute_motor_value() = 0; // this needs to be implemented differently for variable/button throttle
-
+    void throttleRamping(int throttleValue);
+    virtual int readThrottle();
+    void writeToESC(int throttleRead); 
     void read_sensor_value();
     int get_raw_value() const; // should only be used to debug
     int get_motor_value() const;
@@ -15,7 +26,11 @@ public:
 private:
     int m_raw_value;
     int m_motor_value;
-    int m_sensor_pin;
+    int lastMotorWrite;
+    const int minPulseWidth = 1000; // microseconds
+    const int maxPulseWidth = 2000;
+    const double throttleMinVoltage = 0.8;
+    const double throttleMaxVoltage = 4.2;
 };
 
 #endif
--- motor_control/throttle/base_throttle.ino
+++ motor_control/throttle/base_throttle.ino
@@ -1,15 +1,54 @@
 #include "base_throttle.h"
 
-Throttle::Throttle(const int &pin)
+Throttle::Throttle()
 {
-    m_sensor_pin = pin;
-    m_raw_value = 0;
-    m_motor_value = 0;
+    lastMotorWrite = 0;
+}
+
+
+void Throttle::run(){
+    setup();
+    loop();
+}
+
+void Throttle::setup() {
+  // Initialisation of ESC
+  ESC.attach(ESCOutput, minPulseWidth, maxPulseWidth);
+}
+
+void Throttle::writeToESC(int input_pin) { 
+  ESC.write(map(input_pin, 0, 1023, 0, 180));
+}
+
+int Throttle::readThrottle() {
+  /*
+   * Reads the throttle input from analog signal,
+   * Returns 0~1023
+   */
+  int throttleReading = analogRead(throttleIn);
+  int throttleMin = throttleMinVoltage * 1023; 
+  int throttleMax = throttleMaxVoltage * 1023;
+  // Throttle cutoff threshold
+  if (throttleReading < throttleMin) {
+    return 0;
+  } else if (throttleReading < throttleMax) {
+    return map(throttleReading, throttleMin, throttleMax, 0, 1023);
+  } else {
+    return 1023;
+  }
+}
+
+void Throttle::throttleRamping(int throttleValue) {
+  for (int i = lastMotorWrite; i <= throttleValue; i++) {
+    writeToESC(i);
+    // Serial.println(i);
+    delay(map(i, 0, 1023, 30, 1));
+  }
 }
 
 void Throttle::read_sensor_value()
 {
-    m_raw_value = analogRead(m_sensor_pin);
+    m_raw_value = analogRead(throttleIn);
 }
 
 int Throttle::get_raw_value() const
@@ -20,4 +59,16 @@ int Throttle::get_raw_value() const
 int Throttle::get_motor_value() const
 {
     return m_motor_value;
+}
+
+void Throttle::loop() {
+  // Standard Arduino has 10-bit output
+  int throttleRead = readThrottle();
+
+  if (throttleRead > lastMotorWrite) {
+    throttleRamping(throttleRead);
+  } else {
+    writeToESC(throttleRead);
+  }
+  lastMotorWrite = throttleRead;
 }
