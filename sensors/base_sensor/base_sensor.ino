335fa86a92daae7d95a82c7ccaea9179295d63b3
Johnson Zhou <johnson.zhou.sh@icloud.com>
2020-11-24 01:55:04 +0800

    Newly refactored Hall Effect Sensor Code

--- sensors/base_sensor/base_sensor.ino
+++ sensors/base_sensor/base_sensor.ino
@@ -3,10 +3,11 @@
 Sensor::Sensor(const int &pin)
 {
     m_sensor_pin = pin;
-    pinMode(m_sensor_pin, INPUT);
+    // pinMode(m_sensor_pin, INPUT);
     m_raw_value = 0;
     m_sensor_value = 0;
 }
+
 
 double Sensor::get_raw_value() const
 {
