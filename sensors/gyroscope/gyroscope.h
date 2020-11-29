1c84497196ec054b9176343a6d20ccf17165a457
MiraynaMckinney <60904334+MiraynaMckinney@users.noreply.github.com>
2020-11-29 09:05:14 -0800

    Update gyroscope.h

--- sensors/gyroscope/gyroscope.h
+++ sensors/gyroscope/gyroscope.h
@@ -5,6 +5,8 @@ class Gyroscope: public Cartesian_Sensor
 {
 public:
     Gyroscope(const int &pin);
-
+    values.val1.name="Hea:";
+    values.val2.name="Pit:";
+    values.val3.name="Rol:";
     void read_sensor_value();
 };
