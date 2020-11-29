dea5732d5e28a68075bc7600e518d441c60fbb8e
MiraynaMckinney <60904334+MiraynaMckinney@users.noreply.github.com>
2020-11-29 09:04:41 -0800

    Update base_sensor.h

--- sensors/base_sensor/base_sensor.h
+++ sensors/base_sensor/base_sensor.h
@@ -1,6 +1,17 @@
 #ifndef BASE_SENSOR_H
 #define BASE_SENSOR_H
 
+class data_value{
+    string name = 0;
+    double value = 0;
+}
+class data_set{  
+    data_value val1;
+    data_value val2;
+    data_value val3;
+}
+
+
 class Sensor
 {
 public:
@@ -10,11 +21,12 @@ public:
 
     double get_raw_value() const; // should only be used to debug
     double get_sensor_value() const;
-
+   
 protected:
     double m_raw_value;
     double m_sensor_value;
     int m_sensor_pin;
+    data_set values;
 };
 
 #endif //BASE_SENSOR
