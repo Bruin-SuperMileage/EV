9e0d4bda0c4aeff7587036ed0c7dd358653fe0b4
MiraynaMckinney <60904334+MiraynaMckinney@users.noreply.github.com>
2020-11-29 09:07:59 -0800

    Update accelerometer.h

--- sensors/accelerometer/accelerometer.h
+++ sensors/accelerometer/accelerometer.h
@@ -5,6 +5,8 @@ class Accelerometer: public Cartesian_Sensor
 {
 public:
     Accelerometer(const int &pin);
-
+    values.val1.name="MaX:";
+    values.val1.name="MaY:";
+    values.val1.name="MaZ:";
     void read_sensor_value();
 };
