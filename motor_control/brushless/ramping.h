16f602e369c0df7d6d95a2c347e7f4344b10bd0f
Young Sung Lyun <72766090+aspboy217@users.noreply.github.com>
2020-12-23 12:54:19 -0800

    Update ramping.h

--- motor_control/brushless/ramping.h
+++ motor_control/brushless/ramping.h
@@ -1,42 +1,38 @@
 #ifndef RAMPING_H
 #define RAMPING_H
-
 class Ramping{
 public:
   Ramping();
   virtual byte newSpd(int throttle) = 0;
-  
 protected:
-  void setThrottle(int throttle);
   void setCurrentSpd(byte spd); 
   void setDesiredSpd(byte spd);
 
-  int  getThrottle()   const;
   byte getCurrentSpd() const;
   byte getDesiredSpd() const;
+
+  bool isTimeToChangeSpd();
 
   void spdUp(byte amount);
   void spdDown(byte amount);
 private:
-  int  m_throttleIP;
   byte m_currentSpd;
   byte m_desiredSpd;
+  long m_time = 0;
+  const long m_maxWaitTime = 100;
 };
 
 class Linear : public Ramping{
 public:
-  Linear(byte rampSpd);
   byte newSpd(int throttle);
 private:
-  byte m_RAMPSPD;
 };
 
 class PD : public Ramping{
 public:
-  PD(byte Kp, byte Kd);
+  PD();
   byte newSpd(int throttle);
 private:
   byte m_error, m_prevError, m_rateError;
-  byte m_Kp, m_Kd;
 };
 #endif
