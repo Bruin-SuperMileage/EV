c19e68fb5dd8b12040f771f1d1f86d7400773a64
rgimel <56706235+rgimel@users.noreply.github.com>
2021-02-04 14:31:03 -0800

    downramping

--- motor_control/throttle/button_control.ino
+++ motor_control/throttle/button_control.ino
@@ -1,20 +1,31 @@
+#include "button_control.h"
+
+ButtonThrottle::ButtonThrottle(const int &pin, int timeToFull) : Throttle(pin, true)
+{
+  this->m_timeToFull = timeToFull;
+  m_isPressed = false;
+}
+
 int ButtonThrottle::mappingFunction(int time_pressed, m_map_type mapType)
 {
   if(m_isPressed == true)
   {
 if (mapType == LINEAR)
   {
-    return std::min((double)time_pressed / (double)m_timeToFull, 1.0) * m_maxValue;
+    m_speedOfCar = std::min((double)time_pressed / (double)m_timeToFull, 1.0) * m_maxValue;
+    return m_speedOfCar;
   }
   if (mapType == POWER)
   {
     if (time_pressed <= m_timeToFull)
     {
-      return std::pow((double)time_pressed / (double)m_timeToFull, 2) * m_maxValue; // This function uses m_timeToFull to create a dependency on what time we pick and creates an exponential to the power of the 2 with the ratio
+      m_speedOfCar = std::pow((double)time_pressed / (double)m_timeToFull, 2) * m_maxValue;
+      return m_speedOfCar; // This function uses m_timeToFull to create a dependency on what time we pick and creates an exponential to the power of the 2 with the ratio
     }
     else
     {
-      return m_maxValue;
+      m_speedOfCar = m_maxValue;
+      return m_speedOfCar;
     }
   }
   if (mapType == EXPONENTIAL)
@@ -30,13 +41,58 @@ if (mapType == LINEAR)
       }
       if (std::exp(ratio) <= m_maxValue)
       {
-        return std::exp(ratio); //This function has no reliance on m_timeToFull, it simply follows an e exponential curve up til maximum value. Takes about 5.5 seconds to reach max speed(assuming 255 is the value we set)
+        m_speedOfCar = std::exp(ratio);
+        return m_speedOfCar; //This function has no reliance on m_timeToFull, it simply follows an e exponential curve up til maximum value. Takes about 5.5 seconds to reach max speed(assuming 255 is the value we set)
       }
     }
     else
     {
-      return m_maxValue;
+      m_speedOfCar = m_maxValue;
+      return m_speedOfCar;
     }
   }
+  }
+}
+
+int mappingFunctionDeceleration(int time_NotPressed)
+{
+   if(m_isPressed == false)
+  {
+    if(m_speedOfCar ==0)
+    {
+      return m_speedOfCar;
+    }
+  if(time_NotPressed >= 1)
+  {
+    double seconds_conversion = time_notPressed/1000;
+    m_speedOfCar = m_speedOfCar/seconds_conversion;
+  }
+  m_speedOfCar = m_speedOfCar/(double)time_NotPressed;
+  return m_speedOfCar;
+  }
+  return m_speedOfCar;
+}
+
+
+/*
+   * Reads the throttle input from analog signal,
+   * Returns 0~100
+   */
+void ButtonThrottle::compute_motor_value()
+{
+  if (get_raw_value() == 1)
+  {
+    if (!m_isPressed)
+    {
+      m_pressStart = millis();
+    }
+    int timePressed = millis() - m_pressStart;
+    //using a linear mapping function(for now)
+    m_motor_value = mappingFunction(timePressed, LINEAR);
+  }
+  else
+  {
+    m_isPressed = false;
+    m_motor_value = 0;
   }
 }
