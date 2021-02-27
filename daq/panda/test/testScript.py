87efb31a8d99c2c9024385b282aa1a3c704bf0ee
Anay Mehta <anaymehta10@gmail.com>
2021-02-27 11:26:34 -0800

    fixes

--- daq/panda/test/testScript.py
+++ daq/panda/test/testScript.py
@@ -69,14 +69,14 @@ while(num_runs < 50):
   now = datetime.now() #set time
   timeName = now.strftime("%H:%M:%S:%f") [:-3] #set current time
   text = text[:-2]
-  text = text + ";" + timeName + "\r\n" #Something with newline in arduino is stopping this from actually being appended to string
+  text = text + "." + timeName + "\r\n" #Something with newline in arduino is stopping this from actually being appended to string
 
   #Printing inputs to Black Box
   with open(completeName, 'a') as f:
       print(text, file = f)
  
   numValues = 22 #expected number of data inputs
-  data = text.split(".") #splits each data point into a list 
+  data = text.split(";") #splits each data point into a list 
   
   for i in range (0, numValues):
         inputs_dict[prefix[i]] = float(data[i]) #may fail on first run, simply re-run
